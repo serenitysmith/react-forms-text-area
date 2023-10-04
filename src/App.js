c5318742766305fc2f770c1fe0ecfce76d12049e
serenitysmith <serenitysmith24@gmail.com>
2023-10-03 22:06:26 -0400

    text area

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [formData, setFormData] = React.useState(
+    {firstName: "", lastName: "", email: "", comments:""}
+)
+
+ // console log statement to make sure handle change is working 
+console.log(formData.comments)
+
+function handleChange(event) {
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [event.target.name]: event.target.value
+        }
+    })
+}
+
+return (
+    <form>
+        <input
+            type="text"
+            placeholder="First Name"
+            onChange={handleChange}
+            name="firstName"
+            value={formData.firstName}
+        />
+        <input
+            type="text"
+            placeholder="Last Name"
+            onChange={handleChange}
+            name="lastName"
+            value={formData.lastName}
+        />
+        <input
+            type="email"
+            placeholder="Email"
+            onChange={handleChange}
+            name="email"
+            value={formData.email}
+        />
+
+{/* 
+/* /**
+     * Challenge: Add a textarea for "comments" to the form
+     * Make sure to update state when it changes. */}
+
+      {/* // in html whatever you put inside text area becomes its value, its  a larger input box for text  */}
+        {/* <textarea>Whatever you put inside</textarea> */}
+
+        {/* react is a little different,see below  */}
+
+        {/* to meet the challenge we added a coments section to our object array above and added fordata.comments inside the vlaue of th etext area we also dded the same hale  change function as above and for hande chage to work it needs a name hence the name comments */}
+<textarea 
+value={formData.comments}
+placeholder="Comments"
+                onChange={handleChange}
+                name="comments"
+
+/>
+       
+
+        
+
+    </form>
+)
 }
 
 export default App;
